4e3ee3527347463c4e5b10df72ac8a9d0b13cc5c
Rivoryx <xriffkax@gmail.com>
2025-05-08 22:17:53 +0200

    Initial commit

---
+++ thread_pool/main.cpp
@@ -0,0 +1,73 @@
+﻿#include <execution>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <vector>
+
+#include "log_duration.h"
+#include "thread_pool.h"
+
+std::mutex mx;
+namespace fs = std::filesystem;
+
+void GetWholeDirInner(const fs::path& path, std::vector<fs::path>& result, ThreadPool& pool) {
+	{
+		// Protecting from race for result access
+		std::lock_guard lock(mx);
+		result.push_back(path);
+	}
+
+	if (!fs::is_directory(path)) {
+		return;
+	}
+
+	// Parsing current directory
+	for (const auto& dir_entry : fs::directory_iterator(path, fs::directory_options::skip_permission_denied)) {
+		// Running recursion for all subdirs, using ThreadPool
+		if (fs::is_directory(dir_entry)) {
+			pool.Enqueue([dir_entry, &result, &pool] { 
+				GetWholeDirInner(dir_entry.path(), result, pool); 
+			});
+		}
+		else {
+			std::lock_guard lock(mx);
+			result.push_back(dir_entry);
+		}
+	}
+}
+
+std::vector<fs::path> GetWholeDir(const fs::path& path) {
+	std::vector<fs::path> result;
+	ThreadPool pool;
+	{
+		LOG_DURATION(std::string("par"));
+		GetWholeDirInner(path, result, pool);
+		// Waiting for all threads to finish
+		pool.Wait();
+	}
+	std::sort(std::execution::par, std::begin(result), std::end(result));
+	return result;
+}
+
+// Helper function to parse paths from different locales
+static std::string to_utf8(const std::u8string& s) {
+	return std::string(s.begin(), s.end());
+}
+
+// Prints all paths to a stream
+template <class Container>
+void Print(Container& container, std::ostream& out = std::cout) {
+	for (const auto& item : container) {
+		out << to_utf8(item.u8string()) << "\n";
+	}
+	out.flush();
+}
+
+int main() {
+	fs::path path("D:\\");
+	std::vector<fs::path> vector = GetWholeDir(path);
+	std::ofstream of("result.txt", std::ios::binary);
+	Print(vector, of);
+	return 0;
+}
---
+++ thread_pool/thread_pool.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <algorithm>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <ranges>
+#include <thread>
+#include <vector>
+
+//#define _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR
+
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t num_threads = std::thread::hardware_concurrency()) {
+        workers_.reserve(num_threads);
+        for ([[maybe_unused]] size_t _ : std::views::iota(0u, num_threads)) {
+            workers_.emplace_back(&ThreadPool::Worker, this);
+        }
+    }
+
+    ~ThreadPool() {
+        Stop();
+        Wait();
+        std::ranges::for_each(workers_, [](std::thread& t) { 
+            if (t.joinable()) {
+                t.join();
+            }
+        });
+    }
+
+    template <class F>
+    void Enqueue(F&& task) {
+        {
+            std::lock_guard lock(queue_mutex_);
+            tasks_.emplace(std::forward<F>(task));
+        }
+        cv_.notify_one();
+    }
+
+    void Stop() {
+        stop_ = true;
+        cv_.notify_all();
+    }
+
+    void Wait() {
+        std::unique_lock lock(queue_mutex_);
+        // Waining for ALL tasks to finish (in queue + already running)
+        cv_finished_.wait(lock, [this]() {
+            return tasks_.empty() && (active_tasks_ == 0);
+        });
+    }
+
+private:
+    std::mutex queue_mutex_;
+    std::condition_variable cv_;
+    std::condition_variable cv_finished_;
+    std::atomic<bool> stop_ = false;
+    std::atomic<size_t> active_tasks_ = 0;
+
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
+
+    void Worker() {
+        while (true) {
+            std::function<void()> task;
+            {
+                std::unique_lock lock(queue_mutex_);
+                cv_.wait(lock, [this]() { return stop_ || !tasks_.empty(); });
+
+                if (stop_ && tasks_.empty()) {
+                    cv_finished_.notify_all();
+                    return;
+                }
+
+                task = std::move(tasks_.front());
+                tasks_.pop();
+                active_tasks_++;
+            }
+
+            try {
+                task();
+            }
+            catch (...) {
+                --active_tasks_;
+                throw;
+            }
+
+            {
+                std::lock_guard lock(queue_mutex_);
+                active_tasks_--;
+                if (tasks_.empty() && active_tasks_ == 0) {
+                    cv_finished_.notify_all();
+                }
+            }
+        }
+    }
+};

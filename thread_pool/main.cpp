111ceb6e5eb97dcc8a742b2b5c5e8d86708e4a60
Rivoryx <xriffkax@gmail.com>
2025-05-10 17:43:06 +0200

    Change files insertion to a single insertion of a sub vector

--- thread_pool/main.cpp
+++ thread_pool/main.cpp
@@ -10,6 +10,8 @@
 
 std::mutex mx;
 namespace fs = std::filesystem;
+static std::atomic<int> files = 0;
+static std::atomic<int> dirs = 0;
 
 void GetWholeDirInner(const fs::path& path, std::vector<fs::path>& result, ThreadPool& pool) {
 	{
@@ -19,21 +21,31 @@ void GetWholeDirInner(const fs::path& path, std::vector<fs::path>& result, Threa
 	}
 
 	if (!fs::is_directory(path)) {
+		++files;
 		return;
 	}
+	++dirs;
+
+	//Creating temp vector to insert all files with a single insertion later
+	std::vector<fs::path> subdir_files;
 
 	// Parsing current directory
 	for (const auto& dir_entry : fs::directory_iterator(path, fs::directory_options::skip_permission_denied)) {
 		// Running recursion for all subdirs, using ThreadPool
 		if (fs::is_directory(dir_entry)) {
+			++dirs;
 			pool.Enqueue([dir_entry, &result, &pool] { 
 				GetWholeDirInner(dir_entry.path(), result, pool); 
 			});
 		}
 		else {
-			std::lock_guard lock(mx);
-			result.push_back(dir_entry);
+			++files;
+			subdir_files.push_back(dir_entry);
 		}
+	}
+	{
+		std::lock_guard lock(mx);
+		result.insert(std::end(result), std::begin(subdir_files), std::end(subdir_files));
 	}
 }
 
@@ -51,7 +63,7 @@ std::vector<fs::path> GetWholeDir(const fs::path& path) {
 }
 
 // Helper function to parse paths from different locales
-static std::string to_utf8(const std::u8string& s) {
+static std::string from_utf8(const std::u8string& s) {
 	return std::string(s.begin(), s.end());
 }
 
@@ -59,15 +71,20 @@ static std::string to_utf8(const std::u8string& s) {
 template <class Container>
 void Print(Container& container, std::ostream& out = std::cout) {
 	for (const auto& item : container) {
-		out << to_utf8(item.u8string()) << "\n";
+		out << from_utf8(item.u8string()) << "\n";
 	}
 	out.flush();
 }
 
 int main() {
-	fs::path path("D:\\");
+	fs::path path("C:\\Windows\\");
 	std::vector<fs::path> vector = GetWholeDir(path);
 	std::ofstream of("result.txt", std::ios::binary);
 	Print(vector, of);
+	std::cout 
+		<< "For path: \"" << from_utf8(path.u8string()) 
+		<< "\" dirs: " << dirs << " files: " << files 
+		<< " files percentage is: " << static_cast<double>(files) * 100 / (files + dirs) 
+		<< "%" << std::endl;
 	return 0;
 }
